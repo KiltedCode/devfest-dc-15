79a4bee439d52b2d188fb56ff4c8778c1e85995e
RHGeek <john.niedzwiecki.ii@gmail.com>
2015-09-11 23:05:12 -0400

    Added Start Plan Dialog
    Added dialog for starting plan.
    Still needs datepicker to work properly and local storage implementation
    for saving.

--- app/components/pt.hub/pt.hub.ctrls.js
+++ app/components/pt.hub/pt.hub.ctrls.js
@@ -1,9 +1,11 @@
 /* hub controller */
 angular.module('pt.hub')
-.controller('HubCtrl', [ '$scope', 'HubService', function($scope, HubService) {
+.controller('HubCtrl', [ '$scope', '$mdDialog', 'HubService', function($scope, $mdDialog, HubService) {
 	$scope.model = {};
 	$scope.model.myPlansSumm = [];
 	$scope.model.availablePlans = [];
+	$scope.model.planStartDate = '';
+	$scope.model.startAP = null;
 	
 	HubService.getCurrentPlansSummary()
 		.success(function(data, status) {
@@ -22,4 +24,43 @@ angular.module('pt.hub')
 			//TODO: error handling
 			console.log('error', status);
 		});
+
+	var startPlan = function(id, startDate) {
+		//TODO: save to current plans
+	};
+
+	$scope.model.getStarted = function(ev, id, index) {
+		$scope.model.startAP = $scope.model.availablePlans[index];
+	    $mdDialog.show({
+			controller: 'GetStartedCtrl',
+			templateUrl: 'views/partials/dialog-get-started.html',
+			scope: $scope,
+			parent: angular.element(document.body),
+			targetEvent: ev,
+			clickOutsideToClose:false
+	    })
+	    .then(function(startDate) {
+	    	if(startDate != null) {
+	    		startPlan(id, startDate);
+	    	}
+	    }, function() {
+	    	
+	    });
+  	};
+	
+
+}]);
+
+angular.module('pt.hub')
+.controller('GetStartedCtrl', [ '$scope', '$mdDialog', 'HubService', function($scope, $mdDialog, HubService) {
+	$scope.hide = function() {
+		$mdDialog.hide();
+	};
+	$scope.cancel = function() {
+		$mdDialog.cancel();
+	};
+	$scope.start = function() {
+		console.log('start', $scope.model.planStartDate);
+		$mdDialog.hide($scope.model.planStartDate);
+	};
 }]);	
